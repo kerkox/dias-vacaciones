1121e0e49681c6550699a0ea5c09114888790da7
Paul Cortes <paulkerg@gmail.com>
2021-01-15 18:25:28 -0500

    Mejoras visuales

--- pages/index.js
+++ pages/index.js
@@ -22,12 +22,12 @@ export default function Home() {
   const handleSubmit = (e) => {
     e.preventDefault();
     if (inputValue.trim().length > 2) {
-      console.log("inputValue",inputValue)
+      console.log("inputValue", inputValue)
       let dateValue = inputValue.split('-');
-      let date = new Date(dateValue[0], Number(dateValue[1]) -1, dateValue[2]);
-      console.log("date",date)
-      console.log("date String",date.toString())
-      let {fecha:fecha_resultado,dias} = calcularFecha(date, dias);
+      let date = new Date(dateValue[0], Number(dateValue[1]) - 1, dateValue[2]);
+      console.log("date", date)
+      console.log("date String", date.toString())
+      let { fecha: fecha_resultado, dias } = calcularFecha(date, dias);
       let fecha_texto = dateToString(fecha_resultado);
       setFechaFinal(fecha_texto);
       setDias(dias);
@@ -37,23 +37,28 @@ export default function Home() {
     return fecha.format('DD/MMMM/YYYY')
   }
 
- 
+
 
   return (
     <div className={styles.container}>
-      <form onSubmit={handleSubmit}>
-        <TextField id="fecha_inicial" label="Fecha Inicio Vacaciones" type="date" variant="filled" onChange={handleInputChange} name="fecha_inicial" />
-        <TextField id="dias_vacaciones" type="number" label="Cant. Dias Vacaciones" variant="filled" onChange={handleDiasVacaciones} name="dias_vacaciones" />
+      <form onSubmit={handleSubmit} className={styles.grid}>
+        <div className={styles.card}>
+          <TextField id="fecha_inicial" label="Fecha Inicio Vacaciones" type="date" variant="filled" onChange={handleInputChange} name="fecha_inicial" />
+        </div>
+        <div className={styles.card}>
+          <TextField id="dias_vacaciones" type="number" label="Cant. Dias Vacaciones" variant="filled" onChange={handleDiasVacaciones} name="dias_vacaciones" value="15" />
+        </div>
         <br />
         <Button variant="contained" type="submit" color="primary" >Calcular</Button>
         <br />
       </form>
       <div>
-        <p>Dias de vacaciones: {dias}</p>
-        <p>Fecha final de vacaciones: {fechaFinal}</p>
+        <p>Dias de vacaciones: <strong>{dias}</strong></p>
+        <p>Fecha final de vacaciones: <strong>{fechaFinal}</strong></p>
 
       </div>
 
     </div>
   )
 }
+

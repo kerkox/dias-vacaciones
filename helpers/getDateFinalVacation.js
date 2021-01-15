bf8cdec5913e1321c142ce93b62896c0413aeb4c
Paul Cortes <paulkerg@gmail.com>
2021-01-15 18:11:19 -0500

    funcionalidad probada

---
+++ helpers/getDateFinalVacation.js
@@ -0,0 +1,39 @@
+const moment = require('moment');
+const Holidays = require('colombia-holidays');
+
+
+module.exports = calcularFecha = (fechaBaseInput, cantidadDiasVacaciones = 15) => {
+    let fechaBase = moment(fechaBaseInput);
+    let year = fechaBase.year();
+    const festivos = Holidays.getColombiaHolidaysByYear(year);
+
+    for (let x = 0; x < cantidadDiasVacaciones; x++) {
+        fechaBase = fechaBase.add(1, 'd');
+        while (!isWorkDay(festivos, fechaBase)) {
+            fechaBase = fechaBase.add(1, 'd');
+        }
+    }
+    let dias =  daysBetween(moment(fechaBaseInput), fechaBase);
+    return {fecha: fechaBase, dias };
+}
+
+const daysBetween = (fechaInicial, fechaFinal) => {
+    return fechaFinal.diff(fechaInicial, 'days');
+}
+
+const isWorkDay = (festivos, date) => {
+    let day = Number(date.format('d'))
+    switch (day) {
+        case 0:
+        case 6:
+            return false;
+    }
+    return !isHoliday(festivos, date);
+}
+const isHoliday = (festivos, date) => {
+    return festivos.some(day => moment(day.holiday).isSame(date));
+}
+
+const dateToString = (fecha) => {
+    return fecha.format('DD/MMMM/YYYY')
+}
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,59 @@
-import Head from 'next/head'
 import styles from '../styles/Home.module.css'
+import { useState } from 'react';
+import { Button, TextField } from '@material-ui/core';
+import calcularFecha from '../helpers/getDateFinalVacation';
 
 export default function Home() {
+
+  let fecha_inicial;
+
+  const [inputValue, setInputValue] = useState('')
+  const [fechaFinal, setFechaFinal] = useState('')
+  const [dias, setDias] = useState(0);
+
+  const handleInputChange = (e) => {
+    setInputValue(e.target.value);
+  }
+
+  const handleDiasVacaciones = (e) => {
+    setDias(e.target.value);
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (inputValue.trim().length > 2) {
+      console.log("inputValue",inputValue)
+      let dateValue = inputValue.split('-');
+      let date = new Date(dateValue[0], Number(dateValue[1]) -1, dateValue[2]);
+      console.log("date",date)
+      console.log("date String",date.toString())
+      let {fecha:fecha_resultado,dias} = calcularFecha(date, dias);
+      let fecha_texto = dateToString(fecha_resultado);
+      setFechaFinal(fecha_texto);
+      setDias(dias);
+    }
+  }
+  const dateToString = (fecha) => {
+    return fecha.format('DD/MMMM/YYYY')
+  }
+
+ 
+
   return (
     <div className={styles.container}>
-      <label htmlFor="fecha-inicial">Fecha de inicio de vacaciones</label>
-      <input id="fecha-inicial" type="date" />
+      <form onSubmit={handleSubmit}>
+        <TextField id="fecha_inicial" label="Fecha Inicio Vacaciones" type="date" variant="filled" onChange={handleInputChange} name="fecha_inicial" />
+        <TextField id="dias_vacaciones" type="number" label="Cant. Dias Vacaciones" variant="filled" onChange={handleDiasVacaciones} name="dias_vacaciones" />
+        <br />
+        <Button variant="contained" type="submit" color="primary" >Calcular</Button>
+        <br />
+      </form>
+      <div>
+        <p>Dias de vacaciones: {dias}</p>
+        <p>Fecha final de vacaciones: {fechaFinal}</p>
+
+      </div>
+
     </div>
   )
 }
